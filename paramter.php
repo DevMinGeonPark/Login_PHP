ace7dba518d9f0c9b897b789134303a7ef35e034
MInGeonPark <fkarndfksak12@users.noreply.github.com>
2020-09-17 17:34:23 +0900

    one_php_20200917

---
+++ paramter.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    Hello. Ms. <?php echo $_GET['name']; ?> ! <br>
+    You left <?php echo $_GET['address']; ?>
+  </body>
+</html>
