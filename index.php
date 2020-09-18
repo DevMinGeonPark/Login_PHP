2bcb1e14a5dce84493ea4060177b638d39621d5c
MInGeonPark <fkarndfksak12@users.noreply.github.com>
2020-09-18 17:17:39 +0900

    web_200918

--- index.php
+++ index.php
@@ -11,16 +11,15 @@
     <div id="grid">
       <!-- index-->
       <div id="olstyle">
-        <ol>
-          <li><A href="index.php?id=HTML">HTML</a></li>
-          <li><A href="index.php?id=CSS">CSS</a></li>
-          <li><A href="index.php?id=JavaScript">Java</a>Script</li>
-        </ol>
+        <?php
+         ?>
       </div>
       <!-- in -->
       <div id="present">
         <h2> <?php echo $_GET['id']; ?> </h2>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <?php
+        echo file_get_contentS("data/".$_GET['id']);
+         ?>
       </div>
   </div>
   </body>
