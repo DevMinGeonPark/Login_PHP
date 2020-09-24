ea1855f1aff775d831311bb60a14a3f17c98e6e7
MInGeonPark <fkarndfksak12@users.noreply.github.com>
2020-09-24 16:53:02 +0900

    phplogin_IOSNAFSA

--- login.php
+++ login.php
@@ -4,10 +4,6 @@ $id = $_POST['id'];
 $pw = $_POST['pw'];
 $count = file_get_contents('UserData/user.json');
 
-echo json_decode($count,True);
-
-
-echo $count;
 foreach ($count as $key => $value) {
   if ($key==$id) {
     if ($value==$pw) {
--- make_id.php
+++ make_id.php
@@ -1,7 +1,8 @@
 <?php
+
 $user_account->id = $_POST['id'];
 $user_account->pw = $_POST['pw'];
-$user = json_encode($user);
+$user = json_encode($user_account);
 array_push(file_put_contents('UserData/user.json',$user,FILE_APPEND)); // put the data in josn
 header('Location: /index.php');
  ?>
