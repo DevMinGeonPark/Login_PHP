666cdcdeddda21599756d885837ff66f44b350e3
MInGeonPark <fkarndfksak12@users.noreply.github.com>
2020-09-25 15:42:17 +0900

    fsa
    12vsad

--- make_id.php
+++ make_id.php
@@ -1,5 +1,14 @@
 <?php
 
-$id=$_post  
+//$user = array();
+$users = array("id"=>$_POST['id'],"pw"=>$_POST['pw']);
+$users2 = array("id"=>$_POST['id'],"pw"=>$_POST['pw']);
+$a=json_encode($users);
+$b=json_encode($users2);
+array($users, $users2)
 
+echo($a.$b);
+// $user_account = {"id":$_POST['id'],"pw:":$_POST['pw']};
+$userex = json_encode($users);
+file_put_contents('UserData/user.json',$userex,FILE_APPEND);
 ?>
