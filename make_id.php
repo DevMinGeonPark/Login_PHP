f2794b051191ca67cf5782f618b32b67ee80b735
MInGeonPark <fkarndfksak12@users.noreply.github.com>
2020-09-25 10:44:56 +0900

    web
    33

--- make_id.php
+++ make_id.php
@@ -1,8 +1,5 @@
 <?php
 
-$user_account->id = $_POST['id'];
-$user_account->pw = $_POST['pw'];
-$user = json_encode($user_account);
-array_push(file_put_contents('UserData/user.json',$user,FILE_APPEND)); // put the data in josn
-header('Location: /index.php');
- ?>
+$id=$_post  
+
+?>
